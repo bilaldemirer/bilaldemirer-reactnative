e8d34b37022743533ed1ded4661f7092eb5399ff
bilal.demirer <bilal.demirer@boun.edu.tr>
2022-10-12 17:18:23 +0300

    first component

---
+++ src/components/button/button.container.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ButtonUI from './button.ui';
+
+const ButtonContainer = props => {
+  const {} = props;
+
+  return <ButtonUI />;
+};
+
+export default ButtonContainer;
---
+++ src/components/button/button.ui.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {styles} from './button.styles';
+
+const ButtonUI = ({}) => {
+  return;
+};
+
+export default ButtonUI;
